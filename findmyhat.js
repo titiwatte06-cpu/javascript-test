695e96eb19d6a6eda613b3f554e90b3b4acb28d9
Titiwat <titiwat.te06@gmail.com>
2026-04-24 10:37:11 +0700

    added1

---
+++ findmyhat.js
@@ -0,0 +1,19 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+
+
+
+const hatChar = ' ^ ';
+
+const fieldChar = '0';
+
+const fallChar = ' O ';
+
+class Field {
+  constructor(field){
+
+    this._field = field;
+    
+  }
+
+}
