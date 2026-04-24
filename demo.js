81d228a24032208160249e561c7437a9d12d20a4
Titiwat <titiwat.te06@gmail.com>
2026-04-24 14:14:50 +0700

    draft2

---
+++ demo.js
@@ -0,0 +1,18 @@
+import readline from 'readline'
+
+function Field() {
+        for (let y = 0; y < 3; y++) { 
+        const grid = Array.from({ length: 5 }).fill('😍');
+
+        
+        if (y === 0) {
+            grid[0] = '🚩';
+        }
+
+        grid.join('');
+    }
+}
+
+console.log(Field())
+
+
--- findmyhat.js
+++ findmyhat.js
@@ -1,19 +1,55 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-
-
-
-const hatChar = ' ^ ';
-
-const fieldChar = '0';
-
-const fallChar = ' O ';
+const fieldChar = '😍';
+const fieldChar1 = '🔥';
 
 class Field {
   constructor(field){
 
     this._field = field;
-    
+    this._xDist = 0;
+    this._yDist = 0;
+
   }
 
+  print(){
+    for (let i=0; i<this._field.length; ++i){
+        console.log(this._field[i].join(''));
+    }
+  }
+
+  
+  static generateField(x, y, percent){
+
+    let numNodes, area = x * y;
+
+    const randomField = [];
+    for (let i = 0; i < y; i++) {
+        randomField.push([]);
+    }
+    for (let i = 0; i < y; i++) {
+        for (let j = 0; j < x; j++) {
+            if (i === 0 && j === 0) {
+                randomField[0].push(fieldChar1)
+            } else {
+                randomField[i].push(fieldChar);
+            } 
+            
+        }
+    }
+
+    return randomField;
+
+  }
+
+  
 }
+
+
+
+const easyField = new Field(Field.generateField(3, 3, 5));
+easyField.print();
+
+
+
+
